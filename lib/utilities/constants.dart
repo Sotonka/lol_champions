34b53162bb47b2868e0679a597d2714f4b739f95
Evgeny Alekseenko <punconge@gmail.com>
2022-06-16 09:06:51 +1000

    Create constants.dart

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+class Constants {
+  static const String PATH =
+      'http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json';
+  // /champion.json
+  // //champion/Aatrox.json
+  // http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg
+  // http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg
+  static const String PATH_ICON = 'http://openweathermap.org/img/wn/';
+}

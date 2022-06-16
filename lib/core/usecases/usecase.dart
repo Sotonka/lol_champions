5a4cedae310a0badedcf6ccc56af2e3de4d5f060
Evgeny Alekseenko <punconge@gmail.com>
2022-06-16 12:47:48 +1000

    GetAllChampions usecase

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,6 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:lol_champions/core/error/failure.dart';
 
+// юзкейс с параметрами
+// нужен в случае пагинации на главном экране,
+// куда будет передаваться количество чемпионов
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
+}
+
+// юзкейс без параметров
+abstract class UseCaseNoP<Type> {
+  Future<Either<Failure, Type>> call();
 }
--- lib/feature/domain/repositories/champion_repository.dart
+++ lib/feature/domain/repositories/champion_repository.dart
@@ -3,8 +3,8 @@ import 'package:lol_champions/core/error/failure.dart';
 import 'package:lol_champions/feature/domain/entities/champion_entity.dart';
 
 abstract class ChampionRepository {
-  // содержит методы получения списка персонажей и поиска персонажей
+  Future<Either<Failure, List<ChampionEntity>>> getAllChampions();
+  // с пагинацией
+  //Future<Either<Failure, List<ChampionEntity>>> getAllChampions(int page);
 
-  Future<Either<Failure, List<ChampionEntity>>> getAllPersons(int page);
-  Future<Either<Failure, List<ChampionEntity>>> searchPerson(String query);
 }

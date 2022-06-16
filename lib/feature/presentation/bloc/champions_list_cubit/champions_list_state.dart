742a31d012cc801bf96475b3308a7fb6a4bcf781
Evgeny Alekseenko <punconge@gmail.com>
2022-06-16 13:24:24 +1000

    state

---
+++ lib/feature/presentation/bloc/champions_list_cubit/champions_list_state.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+import 'package:lol_champions/feature/domain/entities/champion_entity.dart';
+
+abstract class ChampionState extends Equatable {
+  const ChampionState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+//
+class ChampionEmpty extends ChampionState {
+  @override
+  List<Object?> get props => [];
+}
+
+//
+class ChampionLoading extends ChampionState {
+  @override
+  List<Object?> get props => [];
+}
+
+//
+class ChampionLoaded extends ChampionState {
+  final ChampionEntity champion;
+
+  const ChampionLoaded(this.champion);
+
+  @override
+  List<Object?> get props => [champion];
+}
+
+//
+class ChampionError extends ChampionState {
+  final String message;
+
+  const ChampionError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
